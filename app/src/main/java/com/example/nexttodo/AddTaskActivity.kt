0ed85c4c6d237169534aeb0bb99f82f6123f9fac
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-05-08 11:44:37 +0530

    feat: add task UI

--- app/src/main/java/com/example/nexttodo/AddTaskActivity.kt
+++ app/src/main/java/com/example/nexttodo/AddTaskActivity.kt
@@ -1,11 +1,59 @@
 package com.example.nexttodo
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import java.text.DateFormat
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
 import android.os.Bundle
+import android.widget.Button
 
 class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var datePickerButton: Button
+    private lateinit var backButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+
+        datePickerButton = findViewById(R.id.datePickerButton)
+        backButton = findViewById(R.id.backButton)
+
+
+
+        // Create a date picker builder
+        val datePickerBuilder = MaterialDatePicker.Builder.datePicker()
+
+        // Build the date picker
+        val datePicker = datePickerBuilder.build()
+
+        backButton.setOnClickListener {
+            backToMainActivity(this)
+        }
+
+        // Set the button click listener
+        datePickerButton.setOnClickListener {
+            // Show the date picker dialog when the button is clicked
+            datePicker.show(supportFragmentManager, "DATE_PICKER")
+        }
+
+        // Set the date picker dialog positive button click listener
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            // The selection parameter contains the selected date in milliseconds since epoch
+            // Convert it to a human-readable date string and set it as the button text
+            val dateString = DateFormat.getDateInstance().format(Date(selection))
+            datePickerButton.text = dateString
+        }
+
+
     }
+
+    private fun backToMainActivity(context: Context) {
+        val intent = Intent(context, MainActivity::class.java)
+        context.startActivity(intent)
+    }
+
+
 }
--- app/src/main/java/com/example/nexttodo/MainActivity.kt
+++ app/src/main/java/com/example/nexttodo/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.nexttodo
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var addTaskButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addTaskButton = findViewById(R.id.addTaskBtn)
+
+        addTaskButton.setOnClickListener {
+            startAddTaskActivity(this)
+        }
+
+    }
+
+    private fun startAddTaskActivity(context: Context) {
+        val intent = Intent(context, AddTaskActivity::class.java)
+        context.startActivity(intent)
     }
 }
