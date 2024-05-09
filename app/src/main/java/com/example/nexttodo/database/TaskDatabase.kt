efb4dc314c458716679b7753b5509b4ff75b8ff6
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-05-09 13:17:28 +0530

    feat: database

---
+++ app/src/main/java/com/example/nexttodo/database/TaskDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.nexttodo.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.nexttodo.dao.TaskDao
+import com.example.nexttodo.entities.Task
+
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class TaskDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TaskDatabase? = null
+
+        fun getDatabase(context: Context): TaskDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
