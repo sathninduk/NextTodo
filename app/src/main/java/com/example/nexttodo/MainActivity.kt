8d735213d50699bd603ea984df9bcc9640d4d659
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-05-11 07:46:36 +0530

    feat: no task message

--- app/src/main/java/com/example/nexttodo/MainActivity.kt
+++ app/src/main/java/com/example/nexttodo/MainActivity.kt
@@ -5,7 +5,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var addTaskButton: Button
     private lateinit var tasksList: RecyclerView
+    private lateinit var noTaskMessage: TextView
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,12 +44,23 @@ class MainActivity : AppCompatActivity() {
         val viewModelFactory = TaskViewModelFactory(repository)
         val taskViewModel = ViewModelProvider(this, viewModelFactory).get(TaskViewModel::class.java)
 
+        noTaskMessage = findViewById(R.id.noTasks)
+        noTaskMessage.visibility = View.GONE
+
         val adapter = TaskAdapter(emptyList(), taskViewModel)
         tasksList.adapter = adapter
 
         taskViewModel.getAllTasks().observe(this, Observer { tasks ->
+
             adapter.tasks = tasks
             adapter.notifyDataSetChanged()
+
+            if (tasks.isEmpty()) {
+                noTaskMessage.visibility = View.VISIBLE
+            } else {
+                noTaskMessage.visibility = View.GONE
+            }
+
         })
 
 
